9b366a1ab22fae6a00ad7990dda233f8e2acac16
Fernanda Ponce <ana.ponce-martinez@inria.fr>
2023-08-08 18:15:31 +0200

    some lines on columns to create db

--- src/ibc_api/create_db.py
+++ src/ibc_api/create_db.py
@@ -23,37 +23,33 @@ preproc_df.head()
 # %%
 raw_df.columns
 raw_df["name"]
-raw_df["name"].str.split("/").str[-1].str.split(".").str[-1]
+raw_df.rename(columns={'name': 'path'}, inplace=True)
+
+raw_df["path"].str.split("/").str[-1].str.split(".").str[-1]
 raw_df["file_ext"] = (
-    raw_df["name"].str.split("/").str[-1].str.split(".").str[-1]
+    raw_df["path"].str.split("/").str[-1].str.split(".").str[-1]
 )
 raw_df["file_ext"].unique()
 raw_df["file_ext"].value_counts()
-raw_df[raw_df["file_ext"] == "gz"]["name"].str.split("/").str[-1].str.split(
+raw_df[raw_df["file_ext"] == "gz"]["path"].str.split("/").str[-1].str.split(
     "."
 ).str[-2].value_counts()
-raw_df["tmp"] = (
-    raw_df[raw_df["file_ext"] == "gz"]["name"]
+raw_df["isNII"] = (
+    raw_df[raw_df["file_ext"] == "gz"]["path"]
     .str.split("/")
     .str[-1]
     .str.split(".")
     .str[-2]
 )
-raw_df[raw_df["tmp"] == "nii"]["file_type"] = "NIFTI"
-raw_df[raw_df["file_ext"] == "gz"]["name"].str.split("/").str[-1].str.split(
+raw_df.loc[(raw_df["isNII"] == 'nii'), "isNII"] = "NIFTI"
+
+
+raw_df[raw_df["file_ext"] == "gz"]["path"].str.split("/").str[-1].str.split(
     "_"
 ).str[0]
-raw_df["."] = (
-    raw_df[raw_df["file_ext"] == "gz"]["name"]
-    .str.split("/")
-    .str[-1]
-    .str.split(".")
-    .str[0]
-    .str.split("_")
-    .str[-1]
-)
+
 raw_df["subject"] = (
-    raw_df[raw_df["file_ext"] == "gz"]["name"]
+    raw_df[raw_df["file_ext"].isin(["gz","tsv","bval","bvec"])]["path"]
     .str.split("/")
     .str[-1]
     .str.split(".")
@@ -62,7 +58,7 @@ raw_df["subject"] = (
     .str[0]
 )
 raw_df["session"] = (
-    raw_df[raw_df["file_ext"] == "gz"]["name"]
+    raw_df[raw_df["file_ext"].isin(["gz","tsv","bval","bvec"])]["path"]
     .str.split("/")
     .str[-1]
     .str.split(".")
@@ -70,14 +66,27 @@ raw_df["session"] = (
     .str.split("_")
     .str[1]
 )
-raw_df["modality"].value_counts()
-raw_df["task"] = (
-    raw_df[raw_df["modality"].isin(["bold", "sbref"])]["name"]
-    .str.split("/")
-    .str[-1]
-    .str.split(".")
-    .str[0]
-    .str.split("_")
-    .str[3]
+
+raw_df["modality"] = (
+    raw_df.apply(lambda row: 'doc' if row["file_ext"] == "json" else 
+                 row["path"].split("/")[-1].split(".")[0].split("_")[-1],
+                 axis=1)
 )
-raw_df["MB"] = raw_df["bytes"].div(1024**2)
+raw_df.loc[raw_df["modality"] == 
+           'participants', "modality"] = "doc"
+raw_df.loc[(raw_df["modality"] == 
+            'epi') &   (raw_df["file_ext"] == 'gz'), "modality"] = "fmap"
+raw_df["modality"].value_counts()
+
+raw_df["task"] = (
+    raw_df[raw_df["modality"].isin(["bold", "sbref","events"])]["path"]
+    .str.split("task-")
+    .str[-1]
+    .str.split("_")
+    .str[0]
+)
+raw_df["task"].value_counts()
+
+raw_df["MB"] = raw_df["bytes"].astype(int).div(1024**2)
+
+
