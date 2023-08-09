96bf584442a2af525405545ad6c41147a4c7cb54
Fernanda Ponce <ana.ponce-martinez@inria.fr>
2023-08-09 11:54:53 +0200

    filling task for json files

--- src/ibc_api/create_db.py
+++ src/ibc_api/create_db.py
@@ -1,4 +1,5 @@
 # %%
+import pandas as pd
 import ibc_api.utils as ibc
 
 ibc.authenticate()
@@ -7,8 +8,6 @@ raw = ibc._connect_ebrains("raw")
 preproc = ibc._connect_ebrains("preprocessed")
 
 # %%
-import pandas as pd
-
 raw_df = pd.DataFrame(raw.__dict__["_files"])
 preproc_df = pd.DataFrame(preproc.__dict__["_files"])
 
@@ -23,17 +22,20 @@ preproc_df.head()
 # %%
 raw_df.columns
 raw_df["name"]
+
+# To follow previous db structure, rename name to path
 raw_df.rename(columns={'name': 'path'}, inplace=True)
 
-raw_df["path"].str.split("/").str[-1].str.split(".").str[-1]
+
+# Get the file extension column, either gz, tsv, bval, bvec or json
 raw_df["file_ext"] = (
     raw_df["path"].str.split("/").str[-1].str.split(".").str[-1]
 )
 raw_df["file_ext"].unique()
 raw_df["file_ext"].value_counts()
-raw_df[raw_df["file_ext"] == "gz"]["path"].str.split("/").str[-1].str.split(
-    "."
-).str[-2].value_counts()
+
+# Helper column to know when the file is a NIFTI, maybe not needed since all
+# gz files are NIFTI
 raw_df["isNII"] = (
     raw_df[raw_df["file_ext"] == "gz"]["path"]
     .str.split("/")
@@ -44,19 +46,17 @@ raw_df["isNII"] = (
 raw_df.loc[(raw_df["isNII"] == 'nii'), "isNII"] = "NIFTI"
 
 
-raw_df[raw_df["file_ext"] == "gz"]["path"].str.split("/").str[-1].str.split(
-    "_"
-).str[0]
-
+# Get the subject column from the path
 raw_df["subject"] = (
     raw_df[raw_df["file_ext"].isin(["gz","tsv","bval","bvec"])]["path"]
     .str.split("/")
     .str[-1]
-    .str.split(".")
-    .str[0]
     .str.split("_")
     .str[0]
 )
+
+
+# Get the session column from the path
 raw_df["session"] = (
     raw_df[raw_df["file_ext"].isin(["gz","tsv","bval","bvec"])]["path"]
     .str.split("/")
@@ -67,17 +67,23 @@ raw_df["session"] = (
     .str[1]
 )
 
+
+# Get the modality column from the path (bold, sbref, fmap, doc, etc)
 raw_df["modality"] = (
     raw_df.apply(lambda row: 'doc' if row["file_ext"] == "json" else 
                  row["path"].split("/")[-1].split(".")[0].split("_")[-1],
                  axis=1)
 )
+# Decided to categorize participants.tsv and json files as doc, TBD!
 raw_df.loc[raw_df["modality"] == 
            'participants', "modality"] = "doc"
+# Decided to rename 'epi' files modality as fmap, think it's more accurate
 raw_df.loc[(raw_df["modality"] == 
             'epi') &   (raw_df["file_ext"] == 'gz'), "modality"] = "fmap"
 raw_df["modality"].value_counts()
 
+
+# Get the task column from the path and modality 
 raw_df["task"] = (
     raw_df[raw_df["modality"].isin(["bold", "sbref","events"])]["path"]
     .str.split("task-")
@@ -85,8 +91,18 @@ raw_df["task"] = (
     .str.split("_")
     .str[0]
 )
-raw_df["task"].value_counts()
+# Get the task column for task-reference json files
+task_ref = (raw_df["path"].str.contains("json") & 
+            raw_df["path"].str.contains("task"))
+raw_df.loc[task_ref, "task"] = (
+    raw_df.loc[task_ref, "path"]
+    .str.split("task-")
+    .str[-1]
+    .str.split("_")
+    .str[0]
+)
 
+# Get the file size in MB
 raw_df["MB"] = raw_df["bytes"].astype(int).div(1024**2)
 
 
